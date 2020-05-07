05e655354f98f5f9cb0892d4797e40851eefa2a7
Zhenyu Yang <50858833+yzyjsjem@users.noreply.github.com>
2020-05-08 02:24:06 +1000

    2.0

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-
-from flask import render_template
+from forms import LoginForm
+from flask import render_template, flash, redirect
 
 app = Flask(__name__)
 
@@ -12,3 +12,14 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me={}", format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template('login.html', title="Sign In", form=form)
