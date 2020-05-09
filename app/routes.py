bee72a76721fbbed87cf2e7362971d1dd9726ae3
Besttz <12153492+Besttz@users.noreply.github.com>
2020-05-09 13:12:59 +0000

    1.0.1 Format

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def hello():
     return render_template('index1.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,5 +15,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
