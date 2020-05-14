e0d53f87d7f13368ea9433cce67cc51e2f5cd104
Zhenyu Yang <50858833+yzyjsjem@users.noreply.github.com>
2020-05-15 00:06:22 +1000

    basic user info

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user,  login_required
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 
 @app.route('/')
@@ -48,3 +49,19 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user=User.query.filter_by(username=username).first_or_404()
+    posts =[
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
